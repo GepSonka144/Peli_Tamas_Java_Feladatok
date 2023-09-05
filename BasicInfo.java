df50206d56745b4deb957c004478c15653f271fa
Péli Tamás <szt2peltam@vasvari.org>
2023-09-05 10:38:26 +0200

    BasicInfo.java done

---
+++ BasicInfo.java
@@ -0,0 +1,16 @@
+import java.util.jar.Attributes.Name;
+
+public class BasicInfo {
+    public static void main(String[] args) {
+
+        String Name = "Tamás";
+        int Age = 19;
+        double Heigth = 1.78;
+        boolean IsMarried = false; 
+        System.out.println("Name: "+ Name +"\n"
+                            +"Age: "+ Age +"\n"
+                            +"Heigth: "+ Heigth +"\n"
+                            +"Married: "+ IsMarried
+        );
+    }
+}
