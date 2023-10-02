e1864292b22d247c2d4b187fa10edd356b5f70de
unknown <pelitamas494@gmail.com>
2023-10-02 18:55:20 +0200

    kesz

--- Quadratic.java
+++ Quadratic.java
@@ -1,25 +1,25 @@
 public class Quadratic {
     public static void main(String[] args) {
-        System.out.println(QuadraticSolver(2, -1, -3));
+        System.out.println(QuadraticSolver(1,0,0));
     }
 
 
     public static String QuadraticSolver(double a, double b, double c){
 
-        double D = (b*b) - 4 * a * c;
+        double D = (b*b) - (4 * a * c);
         if(D > 0 ){
-            double x1 = (b + Math.sqrt(D))/(2*a);
-            double x2 = (b - Math.sqrt(D))/(2*a);
+            double x1 = (-b + Math.sqrt(D))/(2*a);
+            double x2 = (-b - Math.sqrt(D))/(2*a);
 
-            return "Az egyenlet megoldása: x1 = " + x1 + " , x2 = " + x2;
+            return "Az egyenlet zérushelyei: x1 = " + x1 + " , x2 = " + x2;
         }
         else if(D == 0){
             double x1 = (b + Math.sqrt(D))/(2*a);
-            return "Az egyenlet megoldása: x = " + x1;
+            return "Az egyenlet zérushelye: x = " + x1;
 
         }
         else{
-            return "Az egyenletnek nincs megoldása";
+            return "Az egyenletnek nincs zérushelye";
         }
 
 
